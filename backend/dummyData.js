9aa2dc95a4b8d79935ca24037c11b18f9ccf45d4
Jemson <jemsonmislani@gmail.com>
2026-04-23 14:12:25 +0800

    Create a JS files for server of backend and data.

---
+++ backend/dummyData.js
@@ -0,0 +1,9 @@
+
+const products = [
+    {id: 1, prodimg: 'Sockwhite.img', name: 'Jem white sock', price: 189.00, size: 'medium'},
+    {id: 2, prodimg: 'Sockblack.img', name: 'Jem black sock', price: 189.00, size: 'medium'},
+]
+
+const cart = [];
+
+module.exports(products, cart);
